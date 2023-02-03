ab8d5621eb13c2c9f447d1f5dcf581d49b78722c
Rozes <Inoknetikondratiev@yande.ru>
2023-02-03 17:27:06 +0700

    1

--- main.py
+++ main.py
@@ -4,25 +4,35 @@ import sys
 import pygame
 import requests
 
-map_request = "http://static-maps.yandex.ru/1.x/?ll=139.060149%2C-25.928213&spn=30.000,30.000&l=sat"
-response = requests.get(map_request)
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
+def createMap(x, y, z, l):
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={x},{y}&z={z}&l={l}"
+    response = requests.get(map_request)
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return pygame.image.load("map.png")
+
+
+x, y = 60, 60
+size = 5
+mapImage = createMap(x, y, size, "map")
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
+screen.blit(mapImage, (0, 0))
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
-    pass
+    screen.fill(pygame.Color("black"))
+    screen.blit(mapImage, (0, 0))
+    pygame.display.flip()
 pygame.quit()
 
-os.remove(map_file)
+os.remove("map.png")
